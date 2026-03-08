92e7dba6255daaa48fbe092174a951cfe84810ad
adhamshoaib <aashoaib2003@gmail.com>
2026-03-09 01:36:23 +0200

    Allow multiple readings in firebase

--- ESP32-firmware/components/http_client/http_client.c
+++ ESP32-firmware/components/http_client/http_client.c
@@ -3,10 +3,10 @@
 
 static const char *TAG = "SMART_METER";
 
- esp_err_t firebase_put_last(time_t ts, float energy_kwh)
+esp_err_t firebase_post_last(time_t ts, float energy_kwh)
 {
     const char *url =
-    "https://test2-8c525-default-rtdb.europe-west1.firebasedatabase.app/test/last.json";
+        "https://test2-8c525-default-rtdb.europe-west1.firebasedatabase.app/test/last.json";
 
     char payload[128];
     snprintf(payload, sizeof(payload),
@@ -15,10 +15,9 @@ static const char *TAG = "SMART_METER";
 
     esp_http_client_config_t config = {
         .url = url,
-        .method = HTTP_METHOD_PUT,
+        .method = HTTP_METHOD_POST,
         .crt_bundle_attach = esp_crt_bundle_attach,
-        .timeout_ms = 8000
-    };
+        .timeout_ms = 8000};
 
     esp_http_client_handle_t client = esp_http_client_init(&config);
 
@@ -26,19 +25,18 @@ static const char *TAG = "SMART_METER";
     esp_http_client_set_post_field(client, payload, strlen(payload));
 
     esp_err_t err = esp_http_client_perform(client);
-    if (err!=ESP_OK)
-    return err;
+    if (err != ESP_OK)
+        return err;
 
-    if (err == ESP_OK) 
+    if (err == ESP_OK)
     {
-    int code = esp_http_client_get_status_code(client);
-    if (code != 200)
-     {
-        ESP_LOGE(TAG, "HTTP error: %d", code);
-        err = ESP_FAIL;
+        int code = esp_http_client_get_status_code(client);
+        if (code != 200)
+        {
+            ESP_LOGE(TAG, "HTTP error: %d", code);
+            err = ESP_FAIL;
+        }
     }
-    }
-    
 
     esp_http_client_cleanup(client);
     return err;
