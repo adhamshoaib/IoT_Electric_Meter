3afa1dafbfc528e92adab522407e6e7dd6ae1af5
adham shoaib <aashoaib2003@gmail.com>
2026-04-12 21:30:15 +0200

    temp

--- ESP32-firmware/components/wifi_sta/wifi_sta.c
+++ ESP32-firmware/components/wifi_sta/wifi_sta.c
@@ -6,3 +6,9 @@ static const char *Tag = "WIFI_STA";
 static esp_netif_t *S_wifi_netif = NULL;
 static EventGroupHandle_t *s_wifi_event_group = NULL;
 static wifi_netif_driver_t wifi_netif_driver = NULL;
+
+static void on_wifi_event(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+
+static void on_ip_event(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+
+static void on_wifi_start(void *esp_netif, esp_event_base_t base, int32_t event_id, void *data);
